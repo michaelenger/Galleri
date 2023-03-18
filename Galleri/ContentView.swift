ba9a1e64de28ad3a31c79f8a91afd8d4c7a3a896
Michael Enger <git@michaelenger.com>
2023-03-18 18:34:26 +0000

    Show a "No Image" when no file path is provided

--- Galleri/ContentView.swift
+++ Galleri/ContentView.swift
@@ -10,18 +10,31 @@ import SwiftUI
 struct ContentView: View {
     var image: NSImage?
 
-    init(filePath: String) {
-        let imageUrl = URL(fileURLWithPath: filePath)
-        self.image = NSImage(contentsOf: imageUrl)
+    init(filePath: String? = nil) {
+        if filePath != nil {
+            let imageUrl = URL(fileURLWithPath: filePath!)
+            self.image = NSImage(contentsOf: imageUrl)
+        } else {
+            self.image = nil
+        }
     }
 
     var body: some View {
-        VStack {
-            if self.image != nil {
-                Image(nsImage: self.image!)
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
+        if self.image != nil {
+            Image(nsImage: self.image!)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+        } else {
+            VStack {
+                Image(systemName: "photo")
+                    .imageScale(.large)
+                    .foregroundColor(.secondary)
+                    .padding(2.0)
+                Text("No Image")
+                    .font(.footnote)
+                    .foregroundColor(.secondary)
             }
+            .padding()
         }
     }
 }
@@ -29,6 +42,8 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView(filePath: "/Users/michaelenger/Downloads/DDfX1SX.jpeg")
-            .frame(width: 500.0, height: 500.0)
+            .frame(width: 200.0, height: 200.0)
+        ContentView()
+            .frame(width: 200.0, height: 200.0)
     }
 }
