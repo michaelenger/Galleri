5adb486fb651a584510fa9a99d71f2ba89692622
Michael Enger <git@michaelenger.com>
2023-03-18 17:15:28 +0000

    Initial Commit

---
+++ Galleri/GalleriApp.swift
@@ -0,0 +1,17 @@
+//
+//  GalleriApp.swift
+//  Galleri
+//
+//  Created by Michael Enger on 18/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct GalleriApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
